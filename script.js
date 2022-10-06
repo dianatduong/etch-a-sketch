31f32ce00fc7d660d715896810f7af7c4fbc9240
Diana Duong <dduong013@gmail.com>
2022-10-05 22:46:18 -0500

    create divs

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector("#container");
-const div = document.createElement("div");
+//const div = document.createElement("div");
 
-div.textContent = "Hello"
+for(let i =0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.style.border = "2px solid black";
+    div.style.width = "20px";
+    div.style.height = "20px";
+    container.append(div);
 
-container.append(div);
+}
+
+
+//container.append(div);
+
+//borderColor()
