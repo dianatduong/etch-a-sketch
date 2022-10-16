500b755e9c3fd055e128a2bbe29d6d1cd5bbb494
Diana Duong <dduong013@gmail.com>
2022-10-16 15:11:48 -0500

    add click event for div

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const container = document.querySelector(".container");
 container.style.display = "grid";
 container.style.height = "20px";
+//container.gridTemplateColumns = "repeat(16, 30px)";
+//container.gridTemplateRows = "repeat(16, 30px)";
 
+const createGrid = () => {
 
-for(let i =0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.style.border = "2px solid black";
-    container.append(div);
+    for(let i =0; i < 256; i++) {
+        const div = document.createElement("div");
+        div.className = "grid";
+        div.style.border = "2px solid black";
+        container.append(div);
+    }
 }
+
+createGrid()
+
+
+//const square = document.querySelector(".grid");
+
+const pixel = document.querySelector("div");
+
+const mouseOver = (event) => {
+    event.target.style.backgroundColor = "red";
+}
+
+//const mouseOut = () => {
+  //  pixel.style.backgroundColor = "green";
+//}
+
+
+pixel.addEventListener("click", mouseOver);
+//pixel.addEventListener("click", mouseOver);
+
 
