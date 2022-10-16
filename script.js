a2fe9fd3458e07745c9b64714cc53a985d14f6f4
Diana Duong <dduong013@gmail.com>
2022-10-16 14:06:07 -0500

    create 16x16 grid

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const container = document.querySelector("#container");
-//const div = document.createElement("div");
+const container = document.querySelector(".container");
+container.style.display = "grid";
+container.style.height = "20px";
+
 
 for(let i =0; i < 256; i++) {
     const div = document.createElement("div");
     div.style.border = "2px solid black";
-    div.style.width = "20px";
-    div.style.height = "20px";
     container.append(div);
-
 }
 
-
-//container.append(div);
-
-//borderColor()
