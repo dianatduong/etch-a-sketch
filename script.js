b217bfe28b83ee57618ccd71eaeacd1441e21fc6
Diana Duong <dduong013@gmail.com>
2022-10-25 12:11:38 -0500

    add button

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ container.style.gridTemplateColumns = "repeat(16,30px)";
 container.style.gridTemplateRows = "repeat(16,30px)";
 
 
-const createGrid = () => {
+const button = document.createElement("button");
+const grid = document.querySelectorAll("div")
 
+container.after(button)
+button.textContent = "Clear";
+
+
+const createGrid = () => {
     for(let i =0; i < 256; i++) {
         const div = document.createElement("div");
         div.className = "grid";
@@ -16,13 +22,20 @@ const createGrid = () => {
 }
 
 
-createGrid()
-
 const mouseOver = (event) => {
     event.target.style.backgroundColor = "red";
 }
 
+
+const clearGrid = () => {
+
+
+}
+
+
+createGrid()
+
+
+button.addEventListener("click", clearGrid)
+
 container.addEventListener("mouseover", mouseOver);
-
-
-
