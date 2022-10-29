7ef468523083ebe54b0b27b5f2e21d498ae7a349
Diana Duong <dduong013@gmail.com>
2022-10-29 12:17:06 -0500

    -

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const header = document.querySelector(".header");
 const container = document.querySelector(".container");
-
-
-const btnClear = document.createElement("button");
+const btnEraser = document.createElement("button");
 const btnRed = document.createElement("button");
 const btnRGB = document.createElement("button");
 const btnSize = document.createElement("button");
 const buttonsContainer = document.querySelector(".buttons")
-btnClear.textContent = "Clear";
+btnEraser.textContent = "Eraser";
 btnRed.textContent = "Red";
 btnRGB.textContent = "RGB";
 btnSize.textContent = "Size";
-buttonsContainer.appendChild(btnClear)
+
 buttonsContainer.appendChild(btnRed)
 buttonsContainer.appendChild(btnRGB)
 buttonsContainer.appendChild(btnSize)
+buttonsContainer.appendChild(btnEraser)
 
 
 
@@ -37,13 +36,40 @@ const createGrid = (row, column) => {
     }
 }
 
-const mouseDown = (event) => {
-    event.target.style.backgroundColor = "red";
-}
-
-//button.addEventListener("click", clearGrid)
-
-container.addEventListener("mousedown", mouseDown);
 
 createGrid(16, 16)
+
+
+//Red Button
+const redColor = () => {
+    const red = (event) => {
+        event.target.style.backgroundColor = "red";
+    }
+    container.addEventListener("mouseover", red); 
+}
+btnRed.addEventListener("click", redColor);
+
+
+//Eraser Button
+const clearGrid = () => {
+    const eraser = (event) => {
+        event.target.style.backgroundColor = "white";
+    }
+    container.addEventListener("mouseover", eraser);
+}
+btnEraser.addEventListener("click", clearGrid);
+
+//Random color generator
+const randomColor = () => {
+    const generateColor = (event) => {
+        const a = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+       const c =  Math.floor(Math.random() * 255);
+        event.target.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+    }
+        container.addEventListener("mouseover", generateColor);
+    }
+    btnRGB.addEventListener("click", randomColor);
+        
+    
 
